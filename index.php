afb4b4b609b5f28536d0c9364b3ab65416569d3f
alessandrolega <alessandrolega24@gmail.com>
2022-12-14 15:30:23 +0100

    completato esercizio post correzione

--- index.php
+++ index.php
@@ -4,20 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP BadWords</title>
 </head>
 <body>
+    <!--CREARE VARIABILE PARAGRAFO PHP-->
     <?php
         $paragrafo= 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa nam ab impedit molestias laboriosam ut incidunt quasi fuga sequi necessitatibus facilis voluptatem, similique qui est vitae, non dolorem ex nihil!';
     ?>
-
+    <!--STAMPARE PARAGRAFO HTML-->
     <p>
         <?php echo $paragrafo ?>
     </p>
-
+    <!--STAMPARE PARAGRAFO E LA SUA LUNGHEZZA-->
     <span>
         <?php echo strlen($paragrafo) ?> caratteri
     </span>
+    <!--CENSURA-->
+    <?php
+    if( empty($_GET['censura']) ){
+        echo 'non ci sono parole da censurare';
+        
+    } else {
+        $paragrafoModificato = str_replace('amet', '***', $paragrafo);
+        echo $paragrafoModificato;
+    }
+    ?>
 
 
 </body>
